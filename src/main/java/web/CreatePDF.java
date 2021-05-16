c43ac2641862d03e711cf92c642afa14456c6d73
Yanushevich <maksim_yanushevich@mail.ru>
2021-05-16 20:34:55 +0500

    Исправление импорта библиотеки jakarta обратно на javax

--- src/main/java/web/CreatePDF.java
+++ src/main/java/web/CreatePDF.java
@@ -3,11 +3,11 @@ package web;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
